03780886feeb38735f1545b1bd76909f608096db
Alexander Arhipov <groatom@ya.ru>
2017-03-30 17:14:52 +0300

    Выполнил второе задание

---
+++ js/stat.js
@@ -0,0 +1,51 @@
+// stats.js
+
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 270);
+  
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+  
+  ctx.fillStyle = '#000'; // black;
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+  
+  var max = -1;
+  var maxIndex = -1;
+  
+  for(var i = 0; i < times.length; i++){
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+  
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
+  
+  var barLength = 40;
+  var indent = 50;
+  var initialX = 120;
+  var initialY = 90;
+  
+  ctx.textBaseline = 'top';
+  for(var i = 0; i < times.length; i++){
+    ctx.fillStyle = 'rgba(255, 0, 0, randomInteger(0.1, 0.9))';
+    ctx.fillRect(initialX + indent * i, initialY + (histogramHeight - times[i] * step), barLength, times[i] * step);
+
+    ctx.fillText(names[i], initialX + indent * i, initialY + histogramHeight + 10);
+
+    ctx.fillText(times[i].toFixed(), initialX + indent * i, initialY + (histogramHeight - times[i] * step) - 20);
+  }
+  
+  var orderOfMyName = names.indexOf('Вы');
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(initialX + indent * orderOfMyName, initialY + (histogramHeight - times[orderOfMyName] * step), barLength, times[orderOfMyName] * step);
+};
